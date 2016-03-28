0645548c806ed0c7f70b8811119d991cfb42e0cf
MinJeong_Kim <yhnb3@naver.com>
2016-03-28 18:58:40 +0900

    ㅇㅇ?

--- Java_Project/src/Sample_0321/Selection.java
+++ Java_Project/src/Sample_0321/Selection.java
@@ -2,31 +2,31 @@ package Sample_0321;
 
 import java.util.*;
 
-public class Selection{
-           
-     public static void main(String[] args){
-              //  TODO Auto-generated method stub
+public class Selection {
 
-             Scanner input = new Scanner(System.in);
-             int in;
-             in = input.nextInt();
-           
-             if(in==0){
-                   System.out.println("in is 0");
-             }else if((in % 2) ==0){
-                   System.out.println("in is even");
-             }else {
-                   System.out.println("in is odd");
-             }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-             switch(in % 2){
-                 case 0 :
-                    System.out.println("in is even");
-                    break;
-                 case 1 :
-                    System.out.println("in is odd");
+		Scanner input = new Scanner(System.in);
+		int in;
+		in = input.nextInt();
 
-             }
+		if (in == 0) {
+			System.out.println("in is 0");
+		} else if ((in % 2) == 0) {
+			System.out.println("in is even");
+		} else {
+			System.out.println("in is odd");
+		}
 
-     }
+		switch (in % 2) {
+		case 0:
+			System.out.println("in is even");
+			break;
+		case 1:
+			System.out.println("in is odd");
+
+		}
+
+	}
 }
--- Java_Project/src/Sample_0321/for_statement.java
+++ Java_Project/src/Sample_0321/for_statement.java
@@ -4,10 +4,10 @@ public class for_statement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		for(int i = 0 ; i < 10 ; i++){
-			System.out.println("Value :" +i);
-			
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Value :" + i);
+
 		}
 
 	}
