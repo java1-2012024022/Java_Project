5ce27d92f8866b8c322f108b3a6b5c834ebdd40a
java1-2012024022 <yhnb3@naver.com>
2016-04-08 11:27:32 +0900

    숙제 !!

--- Java_Project/src/sample_0404/television.java
+++ Java_Project/src/sample_0404/television.java
@@ -1,5 +1,19 @@
 package sample_0404;
 
 public class television {
+	private int channel;
+	private int volume;
+	private boolean onOff;
+	
+	television(int c, int v, boolean o) {
+		channel =c ;
+		volume = v;
+		onOff = o;
+		
+	}
 
+	void print() {
+		System.out.println("체널은 " + channel + "이고 볼륨은 " + volume + "입니다.") ;
+		
+	}
 }
--- Java_Project/src/sample_0404/televisiontest.java
+++ Java_Project/src/sample_0404/televisiontest.java
@@ -4,6 +4,10 @@ public class televisiontest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		television myTv = new television(7, 10, true) ;
+		myTv.print();
+		television yourTv = new television(11, 20, true) ;
+		yourTv.print() ;
 
 	}
 
